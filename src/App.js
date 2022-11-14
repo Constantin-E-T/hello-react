07bbad75dc7dfaab545698229b945fea3b73bbfe
Emilian Con <emiliancon@Emilians-MacBook-Pro-2.local>
2022-11-14 19:54:27 +0000

    Created first component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-
-import './App.css';
+import "./App.css";
+import Employee from "./components/Employee";
 
 function App() {
+  console.log("We are about to list the employees");
+  const showEmployees = true;
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          hello world
-        </p>
-      </header>
+      { console.log('Inside the return') }
+      {showEmployees ? (
+        <>
+          <Employee />
+          <Employee />
+          <Employee />
+          <Employee />
+        </>
+      ) : (
+        <p>You cannot see this page</p>
+      )}
     </div>
   );
 }
- 
+
 export default App;
